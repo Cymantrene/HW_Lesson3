e099315bd6ae0655f06d9c1d92e1266fc08c838b
Cymantrene <rengenhik@gmail.com>
2024-11-03 16:52:22 +0300

    Add files via upload

---
+++ Less3_HW2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <windows.h>
+#include <locale.h>
+
+int main(int argc, char **argv)
+{
+	 setlocale(LC_ALL, "en_US.UTF-8");
+    //SetConsoleCP(1251);
+    //SetConsoleOutputCP(1251);
+
+    int a, b, c, sum, mult;
+    //printf("Please enter three type  int numbers a,b,c with spaces!\n");
+    scanf("%d%d%d", &a, &b, &c);
+    sum = (a + b + c);
+    
+    printf("%d+%d+%d=%d\n",a,b,c,sum);
+	mult = a*b*c;
+	printf("%d*%d*%d=%d\n",a,b,c,mult);
+	return 0;
+}
+
